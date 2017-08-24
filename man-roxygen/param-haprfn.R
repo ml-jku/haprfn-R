754e4ae7fc5520398a59a7d8f604bb7e9046007f
Balazs Bencze <megette_a@yahoo.com>
2017-08-24 12:02:21 +0200

    Remove unused file

--- man-roxygen/param-haprfn.R
+++
@@ -1,8 +0,0 @@
-#' @template param-in-file
-#'
-#' @param sparseMatrixPostfix The posftix for the input matrices.
-#'   Default = "_mat.txt".
-#' @param labelsA Individual names as matrix individuals x 4.
-#' @param prange For intervals indicates its range.
-#' @template sparse-matrix-filter
-#' @template param-intervals
