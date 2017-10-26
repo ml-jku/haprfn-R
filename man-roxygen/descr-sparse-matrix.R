e18f2b94a32e3530c8bf8f480d4006bcc2c9d636
Balazs Bencze <megette_a@yahoo.com>
2017-10-26 10:32:22 +0200

    Fix Sparse Matrix description

--- man-roxygen/descr-sparse-matrix.R
+++ man-roxygen/descr-sparse-matrix.R
@@ -4,8 +4,8 @@
 #'
 #' Sparse matrix format where rows are as follows:
 #' \itemize{
-#'   \item number of non-zero elements (\eqn{nnz})
 #'   \item number of rows (\eqn{nrow})
+#'   \item number of non-zero elements (\eqn{nnz})
 #'   \item row pointers, a list of integers of length \eqn{nrow + 1},
 #'     where the ith element is the number of non-zero elements 
 #'     until the \eqn{i - 1}th row
